25c93fd87d4f802d5a2a32b9d950e96d3ad4432e
Bodzio6978 <Bodziowaty6978@gmail.com>
2021-03-11 10:06:42 +0100

    main activity working

--- app/src/main/java/com/gmail/Bodziowaty6978/MainActivity.kt
+++ app/src/main/java/com/gmail/Bodziowaty6978/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.gmail.Bodziowaty6978
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Switch
-import androidx.annotation.NonNull
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.gmail.Bodziowaty6978.MainFragments.HomeFragment
+import com.gmail.Bodziowaty6978.MainFragments.SearchFragment
+import com.gmail.Bodziowaty6978.MainFragments.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import java.lang.Exception
+
+lateinit var bottomNav : BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,5 +26,36 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val home : HomeFragment = HomeFragment()
+        val search : SearchFragment = SearchFragment()
+        val settings : SettingsFragment = SettingsFragment()
+
+        setFragment(home)
+
+        bottomNav = findViewById(R.id.bottom_nav_main)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> {
+                    setFragment(home)
+                }
+                R.id.ic_search -> {
+                    setFragment(search)
+                }
+                R.id.ic_settings -> {
+                    setFragment(settings)
+                }
+                R.id.ic_add -> {
+                    val intent = Intent(this, AddActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            true
+        }
+    }
+    private fun setFragment(fragment : Fragment){
+        supportFragmentManager.apply {
+            beginTransaction().replace(R.id.fl_main,fragment).commit()
+        }
     }
 }
